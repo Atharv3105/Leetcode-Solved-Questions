04f4146bdebe10a1c3bae02c265ac2b320cca8fa
Atharv3105 <atharvpalekar07@gmail.com>
2026-01-22 22:57:57 +0530

    Time: 7 ms (68.49%), Space: 44 MB (70.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int k = s1.length(), n = s2.length();
+        int k = s1.length();
+        int n = s2.length();
 
-        if (k > n)
-            return false;
+        if (k > n) return false;
 
-        HashMap<Character, Integer> s1Map = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
 
-        // Frequency map of s1
-        for (char ch : s1.toCharArray())
-            s1Map.put(ch, s1Map.getOrDefault(ch, 0) + 1);
-
-        // Prepare initial window of size k-1
-        for (int i = 0; i < k - 1; i++) {
-            char ch = s2.charAt(i);
-            window.put(ch, window.getOrDefault(ch, 0) + 1);
+        for (int i = 0; i < k; i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
         }
 
-        // Slide the window
-        for (int i = k - 1; i < n; i++) {
-            // Add new character
-            char addChar = s2.charAt(i);
-            window.put(addChar, window.getOrDefault(addChar, 0) + 1);
+        if (Arrays.equals(s1Count, windowCount)) return true;
 
-            // Check if window matches s1
-            if (window.equals(s1Map))
-                return true;
+        for (int i = k; i < n; i++) {
+            windowCount[s2.charAt(i) - 'a']++;
+            
+            windowCount[s2.charAt(i - k) - 'a']--;
 
-            // Remove leftmost character
-            char removeChar = s2.charAt(i - k + 1);
-            int freq = window.get(removeChar);
-            if (freq > 1)
-                window.put(removeChar, freq - 1);
-            else
-                window.remove(removeChar);
+            if (Arrays.equals(s1Count, windowCount)) return true;
         }
+
         return false;
     }
 }
