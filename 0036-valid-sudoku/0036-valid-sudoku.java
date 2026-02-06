4d4a71f6c3e4f850748d5c2083b9337b6e70f987
Atharv3105 <atharvpalekar07@gmail.com>
2026-02-06 22:39:26 +0530

    Time: 1 ms (99.74%), Space: 46.2 MB (90.73%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Use HashSets to track digits seen in each row, column, and box
-        HashSet<String> seen = new HashSet<>();
-
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+        
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char number = board[i][j];
+                if (board[i][j] == '.') continue;
                 
-                if (number != '.') {
-                    // Create unique strings to represent the number's presence
-                    // If add() returns false, it means the string already exists
-                    if (!seen.add(number + " in row " + i) ||
-                        !seen.add(number + " in col " + j) ||
-                        !seen.add(number + " in box " + i/3 + "-" + j/3)) {
-                        return false;
-                    }
+                int num = board[i][j] - '1'; // 0 to 8
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                    return false;
                 }
+                
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[boxIndex][num] = true;
             }
         }
-        
         return true;
     }
 }
