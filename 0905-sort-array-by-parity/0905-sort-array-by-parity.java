98e7398f7e60c7641db9922c2b39559f2a732e40
Atharv3105 <atharvpalekar07@gmail.com>
2026-02-08 17:07:14 +0530

    Refactor sortArrayByParity method for clarity

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        
-        while (left < right) {
-            if (nums[left] % 2 > nums[right] % 2) {
-                // Swap if left is odd (1) and right is even (0)
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-            }
+        int l = 0, r = nums.length - 1;
 
-            // Move left forward if it's pointing to an even number
-            if (nums[left] % 2 == 0) left++;
-            // Move right backward if it's pointing to an odd number
-            if (nums[right] % 2 != 0) right--;
+        while (l < r) {
+            if (nums[l] % 2 == 0) {
+                l++;
+            } else if (nums[r] % 2 != 0) {
+                r--;
+            } else {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r--;
+            }
         }
         return nums;
     }
